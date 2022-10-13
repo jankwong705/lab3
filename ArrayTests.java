f24180f647e128810f2e31a24f6a262ae75be350
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2022-10-13 14:47:00 -0700

    fix array examples

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,7 +9,19 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlaceNormArray() {
+    int[] input = {1, 4, 6, 2};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{2, 6, 4, 1}, input);
+  }
 
+  @Test
+  public void testReversedNormArray() {
+    int[] input = {9, 0, 8, 7};
+    assertArrayEquals(new int[]{7, 8, 0, 9}, ArrayExamples.reversed(input));
+  }
+  
   @Test
   public void testReversed() {
     int[] input1 = { };
